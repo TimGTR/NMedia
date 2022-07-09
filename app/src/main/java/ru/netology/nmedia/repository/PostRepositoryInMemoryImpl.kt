25234151f5e006a38a560e9674ed70276c0d137e
Timur Garipov <tr.garipov@yandex.ru>
2022-07-10 02:41:50 +0300

    repositoryImpl

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
@@ -7,37 +7,43 @@ import androidx.lifecycle.MutableLiveData
 class PostRepositoryInMemoryImpl : PostRepository {
     private var posts = listOf(
         Post(
-        id = 1,
-        author = "Timur",
-        content = "Это первый пост в НМедиа",
-        published = "03/07/2022"
-    ),
+            id = 1,
+            author = "Timur",
+            content = "Это первый пост в НМедиа",
+            published = "03/07/2022"
+        ),
         Post(
             id = 2,
             author = "T",
             content = "Это 2 пост в НМедиа",
             published = "03/07/2022"
-        ))
+        )
+    )
 
     private val data = MutableLiveData(posts)
 
     override fun getAll(): LiveData<List<Post>> = data
 
-    override fun likeById(id:Long) {
+    override fun likeById(id: Long) {
+        posts = posts.map { post ->
+            if (post.id != id) post
+            else post.copy(likedByMe = !post.likedByMe)
+        }
+        posts = posts.map { post ->
+            if (post.likedByMe) post.copy(likesCount = post.likesCount + 1)
+            else post.copy(likesCount = post.likesCount - 1)
 
-        post = post.copy(
-            likedByMe = !post.likedByMe
-        )
-        post = post.copy(
-            likesCount = if (post.likedByMe) post.likesCount + 1 else post.likesCount - 1
-        )
-        data.value = post
+        }
+        data.value = posts
     }
 
 
-    override fun getShareCount() {
-        post = post.copy(shareCount = post.shareCount + 1)
-        data.value = post
+    override fun getShareCountById(id: Long) {
+        posts = posts.map { post ->
+            post.copy(shareCount = post.shareCount + 1)
+
+        }
+        data.value = posts
     }
 
 
